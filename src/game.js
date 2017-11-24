ee3a9a0468495cdfb0d9f3c69ca586320f3e836a
Baptiste <baptiste.rigondaud@ensimag.fr>
2017-11-24 18:33:02 +0100

    base of the game (game class)

--- src/game.js
+++ src/game.js
@@ -1,3 +1,30 @@
 window.addEventListener('load', () => {
 	console.log('Starting...');
 });
+
+class Game {
+	/**
+	 * The Game
+	 * @param {Number} gameWidth 
+	 * @param {Number} gameHeight 
+	 */
+	construct(gameWidth, gameHeight) {
+		this.gameWidth = gameWidth;
+		this.gameHeight = gameHeight;
+		this.game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+	}
+
+	/**
+	 * Loading the assets as sprites
+	 */
+	preload(){
+	}
+
+	create(){
+
+	}
+
+	update(){
+
+	}
+}
